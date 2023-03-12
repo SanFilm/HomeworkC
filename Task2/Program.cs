c7dc20d2b6dd15c7ea5f22c06b558dee2ba12ccd
SanFilm <62471072+SanFilm@users.noreply.github.com>
2023-03-12 13:19:06 +0300

    task 2 completed - fixed bug

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,11 +7,19 @@ Console.Write("Введите 1-ое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
+if (a != b)
 {
-  Console.WriteLine($"Результат: {a} > {b}");
+  if (a > b)
+  {
+    Console.WriteLine($"Результат: {a} > {b}");
+  }
+  else
+  {
+    Console.WriteLine($"Результат: {b} > {a}");
+  }
 }
 else
 {
-  Console.WriteLine($"Результат: {b} > {a}");
+  Console.WriteLine($"Результат: {b} = {a}");
 }
+
