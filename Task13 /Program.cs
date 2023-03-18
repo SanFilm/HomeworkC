7c3485f7f81d554ef66c291f9319289498dad346
SanFilm <62471072+SanFilm@users.noreply.github.com>
2023-03-18 13:56:35 +0300

    mplited Task10, Task13, Task15

--- Task13 /Program.cs
+++ Task13 /Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Очистка консоли.
+Console.Clear();
+// Console.WriteLine("Hello, World!");
+
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+/* э
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+*/
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// // int n1 = n / 10 % 10;
+// if (n / 100 == 0)
+//   Console.Write("Третьей цифры нет !!!");
+
+// int n1 = n / 100;
+// int n2 = n1 % 10;
+// Console.Write($"{n},, ");
+// Console.Write($"{n1}, ");
+// Console.WriteLine($"{n2}, ");
+
+// --------------------------------------
+// inet 1
+Console.Write("Введи число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string nText = Convert.ToString(n);
+if (nText.Length > 2)
+{
+  Console.WriteLine("Третья цифра слева: " + nText[2]);
+}
+else
+{
+  Console.WriteLine("Третьей цифры нет.");
+}
+
+// --------------------------------------
+// inet 2
+// 
