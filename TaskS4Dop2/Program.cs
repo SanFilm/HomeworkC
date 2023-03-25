f2816de2f3e3969578e566b0021c0eb265eb90a5
SanFilm <62471072+SanFilm@users.noreply.github.com>
2023-03-25 17:18:52 +0300

    Sem_5 DZ init

--- TaskS4Dop2/Program.cs
+++ TaskS4Dop2/Program.cs
@@ -10,3 +10,32 @@ Console.Clear();
     
 */
 // --------------------------------------------------------------
+
+// Денис
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int countDel = 0, i, j, k, m;
+for (i = 3; i <= n / 2; i += 2)
+{
+  countDel = 0;
+  for (j = 2; j <= Math.Sqrt(i) + 1; j++)
+  {
+    if (i % j == 0)
+      countDel++;
+  }
+  if (countDel == 0)
+  {
+    m = n - i;
+    for (k = 2; k <= Math.Sqrt(m) + 1; k++)
+    {
+      if (m % k == 0)
+        countDel++;
+    }
+    if (countDel == 0)
+    {
+      Console.WriteLine($"{i} {m}");
+      // return;
+    }
+  }
+}
