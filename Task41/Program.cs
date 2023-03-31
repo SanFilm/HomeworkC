7864389181a4fcf58c3a0f6c4e4276b7485f3962
SanFilm <62471072+SanFilm@users.noreply.github.com>
2023-03-31 13:42:16 +0300

    Sem_6 DZ Complited

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -5,12 +5,49 @@ Console.Clear();
 
 // --------------------------------------------------------------
 /*
-Задача : 
+Задача : Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 Пример:
-    
-    
+    0, 7, 8, -2, -2 -> 2
+    -1, -7, 567, 89, 223-> 3
 */
 
+// --var.1 - БЕЗ МАССИВА-----------------------------------------
 
+Console.Write("Введите кол-во элементов: ");
+int m = int.Parse(Console.ReadLine()!);
+  // Console.WriteLine(m);
+int r = 0, cnt=0;
+for (int i = 0; i < m; i++)
+{
+  r = new Random().Next(-10, 10);
+  Console.Write($"{r}, ");
+  if (r>0)
+    cnt++;
+}
+Console.WriteLine($"\n{cnt} - количество значений больше 0\n");
+// --------------------------------------------------------------
+
+// --var.2 - С МАССИВОМ------------------------------------------
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+  array[i] = new Random().Next(-10, 11);              // [-10, 10]
+Console.WriteLine($"[{string.Join(", ", array)}] -> ");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  if (array [i] > 0)
+  {
+    Console.Write($"{i}-");
+    Console.Write($"{array[i]}, ");
+    count++;
+  }
+}
+    Console.WriteLine($" - индексы и их значения больше 0.");
+// Console.WriteLine($"\n{count}");
+Console.WriteLine($"\n{count} - количество значений больше 0\n");
 
 // --------------------------------------------------------------
--- Task43/Program.cs
+++ Task43/Program.cs
@@ -5,12 +5,24 @@ Console.Clear();
 
 // --------------------------------------------------------------
 /*
-Задача : 
+Задача : Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 Пример:
-    
-    
+    b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
+Console.Write("Задайте значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
+double x = (-b2 + b1) / (-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Координаты пересечения двух прямых: X: {Math.Round(x, 2)}, Y: {Math.Round(y, 2)}");
+Console.WriteLine($"\nРешение я нашёл в Инете, как решается и почему не понял ...\n");
 
 // --------------------------------------------------------------
